e6580e863175d0c713751ed79ee07011774214e4
ThaiBui <thaibuixuan34@gmail.com>
2020-07-09 23:33:36 +0700

    Finish mailer for resetpassword

--- api/mailer/forgot_password_mail.go
+++ api/mailer/forgot_password_mail.go
@@ -1,1 +1,79 @@
 package mailer
+
+import (
+	"github.com/matcornic/hermes/v2"
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
+	"net/http"
+	"os"
+)
+
+type sendMail struct {
+}
+
+type SendMailer interface {
+	SendResetPassword(string, string, string, string, string) (*EmailResponse, error)
+}
+
+var (
+	SendMail SendMailer = &sendMail{}
+)
+
+type EmailResponse struct {
+	Status   int
+	RespBody string
+}
+
+func (s *sendMail) SendResetPassword(ToUser string, FromAdmin string, Token string, SendGridKey string, AppEnv string) (*EmailResponse, error) {
+	h := hermes.Hermes{
+		Product: hermes.Product{
+			Name: "Handl3r",
+			Link: "handl3r.com",
+		},
+	}
+	var forgotUrl string
+	if os.Getenv("APP_ENV") == "production" {
+		forgotUrl = "http://handl3r.com/resetpassword/" + Token
+	} else {
+		forgotUrl = "http://localhost:3000/resetpassword/" + Token
+	}
+	email := hermes.Email{
+		Body: hermes.Body{
+			Name: ToUser,
+			Intros: []string{
+				"Welcome to handl3r! Good to see you.",
+			},
+			Actions: []hermes.Action{
+				{
+					Instructions: "Click this link to reset your password",
+					Button: hermes.Button{
+						Color: "#FFFFFF",
+						Text:  "ResetPassword",
+						Link:  forgotUrl,
+					},
+				},
+			},
+			Outros: []string{
+				"Need help, or have questions? Just reply to this email, we'd love to help.",
+			},
+		},
+	}
+	emailBody, err := h.GenerateHTML(email)
+	if err != nil {
+		return nil, err
+	}
+
+	from := mail.NewEmail("hand3r", FromAdmin)
+	subject := "ResetPassword"
+	to := mail.NewEmail("Reset Password", ToUser)
+	message := mail.NewSingleEmail(from, subject, to, emailBody, emailBody)
+	client := sendgrid.NewSendClient(SendGridKey)
+	_, err = client.Send(message)
+	if err != nil {
+		return nil, err
+	}
+	return &EmailResponse{
+		Status:   http.StatusOK,
+		RespBody: "Success, Please click on the link provided in your email",
+	}, nil
+}
--- go.mod
+++ go.mod
@@ -7,7 +7,10 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.6.3
 	github.com/jinzhu/gorm v1.9.14
+	github.com/matcornic/hermes/v2 v2.1.0
 	github.com/myesui/uuid v1.0.0 // indirect
+	github.com/sendgrid/rest v2.6.0+incompatible // indirect
+	github.com/sendgrid/sendgrid-go v3.6.0+incompatible
 	github.com/twinj/uuid v1.0.0
 	golang.org/x/crypto v0.0.0-20191205180655-e7c4368fe9dd
 	gopkg.in/stretchr/testify.v1 v1.2.2 // indirect
